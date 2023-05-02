8183fb5a538e265f7209fb8ea9c434fec942d332
Mario <mperezgarcia.guadalupe@alumnado.fundacionloyola.net>
2023-05-02 19:15:46 +0200

    formulario de conexion correcto y funcionando

--- Tareas DWES/TAR-01.05_formulario_conexion/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-define('SERVER', '2daw.esvirgua.com');
-define('USU', 'user2daw_19');
-define('CONTRA', 'M^wPDCC_z3~0');
-define('BBDD', 'user2daw_BD1-19');
--- Tareas DWES/TAR-01.05_formulario_conexion/prueba.php
+++
@@ -1,5 +0,0 @@
-<?php
-$var= "     Hola   .     Mundo";
-$var2=$var.trim();
-echo $var.'<br/>';
-echo $var2.'<br/>';
